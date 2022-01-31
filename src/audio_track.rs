9c73118dff88a0128f3bc395e3edadbeab520125
Lunae Somnia <lunaesomnia@protonmail.com>
2022-01-31 16:49:28 +0100

    [feat] Linear Volume smoothing and small refactoring

--- src/audio_track.rs
+++ src/audio_track.rs
@@ -4,6 +4,8 @@ use hound::WavReader;
 
 use crate::{audio_file::AudioFile, wavreader::WavSampleReader};
 
+const VOLUME_STEPS: usize = 10000;
+
 #[derive(Clone, Copy, PartialEq)]
 pub enum SampleFormat {
     F32,
@@ -38,8 +40,9 @@ pub struct AudioTrack {
     sample_format: SampleFormat,
     #[allow(dead_code)]
     sample_rate: u32,
+    channels: u16,
     holder: WavReader<BufReader<File>>,
-    volume: f32,
+    ctx: TrackCtx,
 }
 
 impl AudioTrack {
@@ -49,13 +52,23 @@ impl AudioTrack {
         let spec = holder.spec();
         let sf = SampleFormat::from((spec.sample_format, spec.bits_per_sample));
         let sr = spec.sample_rate;
+        let ch = spec.channels;
 
         Self {
             name: name.to_string(),
             sample_format: sf,
             sample_rate: sr,
+            channels: ch,
             holder,
-            volume: 0.8,
+            ctx: TrackCtx {
+                current_volume: 0.8,
+
+                volume_counter: 0,
+                volume_step: 1.0,
+                target_volume: 0.8,
+
+                panning: 0.5,
+            },
         }
     }
 
@@ -64,14 +77,24 @@ impl AudioTrack {
     }
 
     pub fn advance(&mut self) -> f32 {
+        let volume_calc = |ctx: &mut TrackCtx| {
+            if ctx.volume_counter > 0 {
+                ctx.volume_counter -= 1;
+                ctx.current_volume += ctx.volume_step;
+            }
+            return ctx.current_volume;
+        };
+
         match self.create_samples().read_next() {
-            Some(x) => x * self.volume,
+            Some(x) => x * volume_calc(&mut self.ctx),
             None => 0.0,
         }
     }
 
-    pub fn set_volume(&mut self, new_volume: f32) {
-        self.volume = new_volume;
+    pub fn set_volume(&mut self, target_volume: f32) {
+        self.ctx.target_volume = target_volume;
+        self.ctx.volume_step = (target_volume - self.ctx.current_volume) / VOLUME_STEPS as f32;
+        self.ctx.volume_counter = VOLUME_STEPS as u16;
     }
 
     fn create_samples(&mut self) -> WavSampleReader {
@@ -93,4 +116,17 @@ impl PartialEq for AudioTrack {
         //&& self.holder == other.holder
         //&& self.volume == other.volume
     }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct TrackCtx {
+    // Volume
+    current_volume: f32,
+
+    // Linear Volume Smoothing
+    target_volume: f32,
+    volume_step: f32,
+    volume_counter: u16,
+
+    panning: f32,
 }
